b4b9fb866fe3aef7eea9235b1f3f0493838088b0
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-06-12 12:05:33 +0900

    Initial Commit

---
+++ SwiftUI-Section5-10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Section5-10
+//
+//  Created by Hiroaki Ishiga on 2021/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Section5-10/SwiftUI_Section5_10App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Section5_10App.swift
+//  SwiftUI-Section5-10
+//
+//  Created by Hiroaki Ishiga on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Section5_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
